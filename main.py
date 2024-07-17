b213aec18a476d50a7b94e9dbc28aab97cda4b13
Jason Kneen <jason.kneen@bouncingfish.com>
2024-07-18 00:01:14 +0100

    Remove two console print statements in chat_with_claude function.

--- main.py
+++ main.py
@@ -612,8 +612,6 @@ async def chat_with_claude(user_input, image_path=None, current_iteration=None, 
         elif chunk.type == "content_block_stop":
             console.print()  # Add a newline at the end of the response
 
-    console.print("\n")
-
     for tool_use in tool_uses:
         tool_name = tool_use.name
         tool_input = tool_use.input
