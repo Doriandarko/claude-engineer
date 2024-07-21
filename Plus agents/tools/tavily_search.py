9f9d89b628f76e72cf224baa88b760c0d3542309
inzi <889113+inzi@users.noreply.github.com>
2024-07-20 20:27:51 -0500

    fixed tavily_search tool

--- Plus agents/tools/tavily_search.py
+++ Plus agents/tools/tavily_search.py
@@ -1,10 +1,15 @@
 import json
+import os
 from tavily import tavily
+from tavily import TavilyClient
 from tools.base_tool import base_tool
+from config import TAVILY_API_KEY
 
 class tavily_search(base_tool):
     def __init__(self):
         super().__init__()
+        api_key=TAVILY_API_KEY
+        self.tavily = TavilyClient(api_key)
         self.definition = {
         "name": "tavily_search",
         "description": "Perform a web search using Tavily API to get up-to-date information or additional context. Use this when you need current information or feel a search could provide a better answer.",
@@ -24,7 +29,7 @@ class tavily_search(base_tool):
     def execute(self, tool_input):
         try:
             query = tool_input["query"]
-            response = tavily.qna_search(query=query, search_depth="advanced")
+            response = self.tavily.qna_search(query=query, search_depth="advanced")
             return json.dumps(response, indent=2)
         except Exception as e:
             return f"Error performing search: {str(e)}"
